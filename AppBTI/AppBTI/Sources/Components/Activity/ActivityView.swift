7db9d80719a3e50a35d123325050c9f554ef9af8
rlarldud1234 <rlarldud6482@naver.com>
2023-01-15 02:06:11 +0900

    feat :: 데이터 공유하는 창

---
+++ AppBTI/AppBTI/Sources/Components/Activity/ActivityView.swift
@@ -0,0 +1,10 @@
+import UIKit
+import SwiftUI
+
+struct ActivityView: UIViewControllerRepresentable {
+    let text: String
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ActivityView>) -> UIActivityViewController {
+        return UIActivityViewController(activityItems: [text], applicationActivities: nil)
+    }
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: UIViewControllerRepresentableContext<ActivityView>) {}
+}
